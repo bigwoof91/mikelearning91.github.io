76995b40766ac0661b9b5f998b5affa27d70fbcb
Michael Wolf <Michael Wolf>
2017-02-25 14:39:54 -0500

    updates

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
-var form = $("#form");
-var search = $("#search").val().trim();
-var numRecords = $("#numRecords").val().trim();
+$(document).ready(function(){ 
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-    'api-key': "4d60067980d842748f25429c4d6503bf",
-    'q': search
-});
 
-$.ajax({
-    url: url,
-    method: 'GET',
-}).done(function(result) {
-    console.log(result);
-}).fail(function(err) {
-    throw err;
-});
-var resDocs = result.docs
 
-for(var i = 0; i < resDocs; i++) {
-	console.log(res[i]);
+$(document).on("click", "#search-button", searchFunction);
+
+function searchFunction(event) {
+	event.preventDefault();
+	var form = $("#form");
+    var search = $("#search").val().trim();
+    var number = $("#number").val().trim();
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+        'api_key': "4d60067980d842748f25429c4d6503bf",
+        'q': search,
+        'page': number
+    });
+    console.log(url);
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function(response) {
+        console.log(response);
+        $("#holder").empty();
+        var resDocs = response.response.docs.length;
+
+
+        for (var i = 0; i < response.response.docs.length; i++) {
+            console.log(resDocs[i]);
+            var itemHolder = $("#article");
+        	var headline = "<h2>" + response.response.docs[i].headline.main + "</h2>";
+        	var leadP = "<p>" + response.response.docs[i].lead_paragraph + "</p>";
+            $("#holder").prepend(itemHolder).append(headline);
+       
+
+            // "<div class='article'><h2>" + response.response.docs[i].headline.main + "</h2><br>" + 
+            // 	response.response.docs[i].lead_paragraph +	"</div>");
+        };
+
+    }).fail(function(err) {
+        throw err;
+    });
 }
 
+$("#holder").append()
+
+
+
+});
