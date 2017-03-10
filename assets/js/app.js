3c253cd51ebeac0b091c46c5c5ce519e8df3dfec
Michael Wolf <Michael Wolf>
2017-03-10 02:19:36 -0500

    selfie UID

--- assets/js/app.js
+++ assets/js/app.js
@@ -149,7 +149,25 @@ download_photo_btn.addEventListener("click", function(e) {
 
     var file = blob; // use the Blob or File API
     var uploadTask = selfieImagesRef.put(file);
+  
+  selfieImagesRef.getDownloadURL().then(function(url) {
+  // `url` is the download URL for 'images/stars.jpg'
 
+  // This can be downloaded directly:
+  var xhr = new XMLHttpRequest();
+  xhr.responseType = 'blob';
+  xhr.onload = function(event) {
+    var blob = xhr.response;
+  };
+  xhr.open('GET', url);
+  xhr.send();
+
+  // Or inserted into an <img> element:
+  var img = document.getElementById('myimg');
+  img.src = url;
+}).catch(function(error) {
+  // Handle any errors
+});
 
 });
 
