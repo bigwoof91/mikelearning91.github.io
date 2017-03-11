c18355ed2e405d292fa42316665b3f996c89689f
Michael Wolf <Michael Wolf>
2017-03-10 22:04:59 -0500

    trying to get image url

--- assets/js/app.js
+++ assets/js/app.js
@@ -150,18 +150,19 @@ download_photo_btn.addEventListener("click", function(e) {
     var file = blob; // use the Blob or File API
     var uploadTask = selfieImagesRef.put(file);
   
-      selfieImagesRef.getDownloadURL().then(function(url) {
-        // `url` is the download URL for 'images/stars.jpg'
-        console.log(url);
-        // This can be downloaded directly:
-        var xhr = new XMLHttpRequest();
-        xhr.responseType = 'blob';
-        xhr.onload = function(event) {
-          var blob = xhr.response;
-        };
-        xhr.open('GET', url);
-        xhr.send();
-      });
+    var handlesImgSaved = selfieImagesRef.getDownloadURL();
+
+
+    // `url` is the download URL for 'images/stars.jpg'
+    $.ajax({
+        method: 'POST',
+        address: 'https://mikelearning91.github.com/assets/',
+        data: handlesImgSaved,
+        contentType: false,
+        processData: false,
+        cache: false
+    });
+        
       //----------------------- trying to get downloadURL to send to microsoft service -----------------------//
       // when sending downloadURL to microsoft I am getting an error saying "FailedToDownloadImage" 400 error bad request
       //-----------------------START-----------------------
